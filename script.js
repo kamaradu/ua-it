630d3b4e941d2df201360319499182f21542b017
kamaradu <bezruk10@gmail.com>
2026-04-24 18:16:29 +0200

    Reduce delay in speak function from 4000ms to 500ms

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ async function speak(id) {
   if (stopFlag) return;
 
   await playAudio(`audio/${id}_ua.mp3`);
-  await delay(4000);
+  await delay(500);
   await playAudio(`audio/${id}_it.mp3`);
 }
 
